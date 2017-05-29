170ed75924b4537e91a878dcab4524ffe92de0b8
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-05-30 02:46:11 +0900

    Create TwitterOAuthActivity

---
+++ app/src/main/java/com/masahirosaito/pictureslistview/TwitterOAuthActivity.kt
@@ -0,0 +1,12 @@
+package com.masahirosaito.pictureslistview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TwitterOAuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitter_oauth)
+    }
+}
