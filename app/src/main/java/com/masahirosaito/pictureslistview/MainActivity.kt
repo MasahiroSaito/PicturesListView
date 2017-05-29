93cb1b8a4ed2a77e6a047268e3ba59757fed2645
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-05-30 02:46:37 +0900

    Launch TwitterOAuthActivity

--- app/src/main/java/com/masahirosaito/pictureslistview/MainActivity.kt
+++ app/src/main/java/com/masahirosaito/pictureslistview/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.masahirosaito.pictureslistview
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!TwitterUtils.hasAccessToken(this)) {
+            val intent = Intent(this, TwitterOAuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
