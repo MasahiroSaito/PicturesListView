4a9c20bd2b408155443b8347776b7db6bbc3bb12
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-05-30 23:36:55 +0900

    Create extensions

---
+++ app/src/main/java/com/masahirosaito/pictureslistview/extensions.kt
@@ -0,0 +1,16 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package com.masahirosaito.pictureslistview
+
+import android.content.Context
+import android.support.annotation.IdRes
+import android.view.View
+import android.widget.Toast
+
+fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
+    findViewById(id) as T
+}
+
+fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
